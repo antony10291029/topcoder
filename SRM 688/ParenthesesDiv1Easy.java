26e8116adef2e771b2f523f779bc98a1f2598f66
Qingyu Li <liqingyu.lqy@gmail.com>
2016-06-18 17:18:46 +0800

    daily practice in the ZJU library

---
+++ SRM 688/ParenthesesDiv1Easy.java
@@ -0,0 +1,112 @@
+import java.io.*;
+import java.util.*;
+
+public class ParenthesesDiv1Easy {
+	public int[] correct(String s) {
+		return new int[0];
+	}
+
+// CUT begin
+	public static void main(String[] args){
+		System.err.println("ParenthesesDiv1Easy (250 Points)");
+		System.err.println();
+		HashSet<Integer> cases = new HashSet<Integer>();
+        for (int i = 0; i < args.length; ++i) cases.add(Integer.parseInt(args[i]));
+        runTest(cases);
+	}
+
+	static void runTest(HashSet<Integer> caseSet) {
+	    int cases = 0, passed = 0;
+	    while (true) {
+	    	String label = Reader.nextLine();
+	    	if (label == null || !label.startsWith("--"))
+	    		break;
+
+            String s = Reader.nextLine();
+            Reader.nextLine();
+            int[] __answer = new int[Integer.parseInt(Reader.nextLine())];
+            for (int i = 0; i < __answer.length; ++i)
+                __answer[i] = Integer.parseInt(Reader.nextLine());
+
+            cases++;
+            if (caseSet.size() > 0 && !caseSet.contains(cases - 1))
+                continue;
+    		System.err.print(String.format("  Testcase #%d ... ", cases - 1));
+
+            if (doTest(s, __answer))
+                passed++;
+	    }
+	    if (caseSet.size() > 0) cases = caseSet.size();
+        System.err.println(String.format("%nPassed : %d/%d cases", passed, cases));
+
+        int T = (int)(System.currentTimeMillis() / 1000) - 1466236078;
+        double PT = T / 60.0, TT = 75.0;
+        System.err.println(String.format("Time   : %d minutes %d secs%nScore  : %.2f points", T / 60, T % 60, 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT))));
+	}
+
+	static boolean doTest(String s, int[] __expected) {
+        s = new String(s);
+		long startTime = System.currentTimeMillis();
+		Throwable exception = null;
+		ParenthesesDiv1Easy instance = new ParenthesesDiv1Easy();
+		int[] __result = new int[0];
+		try {
+			__result = instance.correct(s);
+		}
+		catch (Throwable e) { exception = e; }
+		double elapsed = (System.currentTimeMillis() - startTime) / 1000.0;
+
+		if (exception != null) {
+			System.err.println("RUNTIME ERROR!");
+			exception.printStackTrace();
+			return false;
+		}
+		else if (equals(__result, __expected)) {
+			System.err.println("PASSED! " + String.format("(%.2f seconds)", elapsed));
+			return true;
+		}
+		else {
+			System.err.println("FAILED! " + String.format("(%.2f seconds)", elapsed));
+			System.err.println("           Expected: " + toString(__expected));
+			System.err.println("           Received: " + toString(__result));
+			return false;
+		}
+	}
+
+	static boolean equals(int[] a, int[] b) {
+		if (a.length != b.length) return false;
+		for (int i = 0; i < a.length; ++i) if (a[i] != b[i]) return false;
+		return true;
+	}
+
+	static String toString(int[] arr) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("[ ");
+		for (int i = 0; i < arr.length; ++i) {
+			if (i > 0) sb.append(", ");
+			sb.append(arr[i]);
+		}
+		return sb.toString() + " ]";
+	}
+
+	static class Reader {
+        private static final String dataFileName = "/Users/resry/topcoder/SRM 688/ParenthesesDiv1Easy.sample";
+	    private static BufferedReader reader;
+
+	    public static String nextLine() {
+	        try {
+                if (reader == null) {
+                    reader = new BufferedReader(new InputStreamReader(new FileInputStream(dataFileName)));
+                }
+                return reader.readLine();
+	        }
+	        catch (IOException e) {
+	            System.err.println("FATAL!! IOException");
+	            e.printStackTrace();
+	            System.exit(1);
+	        }
+	        return "";
+	    }
+	}
+// CUT end
+}
