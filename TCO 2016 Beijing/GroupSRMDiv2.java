59bf11011aab43b25bd47d45b9c0b790d2b17ddb
Qingyu Li <liqingyu.lqy@gmail.com>
2016-07-10 09:18:19 +0800

    daily practice at home

---
+++ TCO 2016 Beijing/GroupSRMDiv2.java
@@ -0,0 +1,110 @@
+import java.io.*;
+import java.util.*;
+
+public class GroupSRMDiv2 {
+	public int FindGroups(int[] t) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i : t) {
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 1);
+            }
+        }
+        int ans = 0;
+        for (Map.Entry<Integer, Integer> i : map.entrySet()) {
+            if (i.getValue() % i.getKey() != 0) {
+                return -1;
+            }
+            ans += i.getValue() / i.getKey();
+        }
+		return ans;
+	}
+
+// CUT begin
+	public static void main(String[] args){
+		System.err.println("GroupSRMDiv2 (250 Points)");
+		System.err.println();
+		HashSet<Integer> cases = new HashSet<Integer>();
+        for (int i = 0; i < args.length; ++i) cases.add(Integer.parseInt(args[i]));
+        runTest(cases);
+	}
+
+	static void runTest(HashSet<Integer> caseSet) {
+	    int cases = 0, passed = 0;
+	    while (true) {
+	    	String label = Reader.nextLine();
+	    	if (label == null || !label.startsWith("--"))
+	    		break;
+
+            int[] t = new int[Integer.parseInt(Reader.nextLine())];
+            for (int i = 0; i < t.length; ++i)
+                t[i] = Integer.parseInt(Reader.nextLine());
+            Reader.nextLine();
+            int __answer = Integer.parseInt(Reader.nextLine());
+
+            cases++;
+            if (caseSet.size() > 0 && !caseSet.contains(cases - 1))
+                continue;
+    		System.err.print(String.format("  Testcase #%d ... ", cases - 1));
+
+            if (doTest(t, __answer))
+                passed++;
+	    }
+	    if (caseSet.size() > 0) cases = caseSet.size();
+        System.err.println(String.format("%nPassed : %d/%d cases", passed, cases));
+
+        int T = (int)(System.currentTimeMillis() / 1000) - 1468199534;
+        double PT = T / 60.0, TT = 75.0;
+        System.err.println(String.format("Time   : %d minutes %d secs%nScore  : %.2f points", T / 60, T % 60, 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT))));
+	}
+
+	static boolean doTest(int[] t, int __expected) {
+		long startTime = System.currentTimeMillis();
+		Throwable exception = null;
+		GroupSRMDiv2 instance = new GroupSRMDiv2();
+		int __result = 0;
+		try {
+			__result = instance.FindGroups(t);
+		}
+		catch (Throwable e) { exception = e; }
+		double elapsed = (System.currentTimeMillis() - startTime) / 1000.0;
+
+		if (exception != null) {
+			System.err.println("RUNTIME ERROR!");
+			exception.printStackTrace();
+			return false;
+		}
+		else if (__result == __expected) {
+			System.err.println("PASSED! " + String.format("(%.2f seconds)", elapsed));
+			return true;
+		}
+		else {
+			System.err.println("FAILED! " + String.format("(%.2f seconds)", elapsed));
+			System.err.println("           Expected: " + __expected);
+			System.err.println("           Received: " + __result);
+			return false;
+		}
+	}
+
+	static class Reader {
+        private static final String dataFileName = "/Users/resry/topcoder/TCO 2016 Beijing/GroupSRMDiv2.sample";
+	    private static BufferedReader reader;
+
+	    public static String nextLine() {
+	        try {
+                if (reader == null) {
+                    reader = new BufferedReader(new InputStreamReader(new FileInputStream(dataFileName)));
+                }
+                return reader.readLine();
+	        }
+	        catch (IOException e) {
+	            System.err.println("FATAL!! IOException");
+	            e.printStackTrace();
+	            System.exit(1);
+	        }
+	        return "";
+	    }
+	}
+// CUT end
+}
