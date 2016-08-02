fd01f292a85790b4c3886bc4a711eaf5eb012bfa
Qingyu Li <liqingyu.lqy@gmail.com>
2016-08-02 08:45:45 +0800

    daily practice at home

---
+++ TCO 2016 NYC/EllysCandyBags.java
@@ -0,0 +1,107 @@
+import java.io.*;
+import java.util.*;
+
+public class EllysCandyBags {
+	public int getSize(String[] packets) {
+        int n = packets.length;
+        int[] a = new int[26];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < packets[i].length(); ++j) {
+                ++a[packets[i].charAt(j) - 'A'];
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < 26; ++i) res += a[i] / 2;
+		return res;
+	}
+
+// CUT begin
+	public static void main(String[] args){
+		System.err.println("EllysCandyBags (250 Points)");
+		System.err.println();
+		HashSet<Integer> cases = new HashSet<Integer>();
+        for (int i = 0; i < args.length; ++i) cases.add(Integer.parseInt(args[i]));
+        runTest(cases);
+	}
+
+	static void runTest(HashSet<Integer> caseSet) {
+	    int cases = 0, passed = 0;
+	    while (true) {
+	    	String label = Reader.nextLine();
+	    	if (label == null || !label.startsWith("--"))
+	    		break;
+
+            String[] packets = new String[Integer.parseInt(Reader.nextLine())];
+            for (int i = 0; i < packets.length; ++i)
+                packets[i] = Reader.nextLine();
+            Reader.nextLine();
+            int __answer = Integer.parseInt(Reader.nextLine());
+
+            cases++;
+            if (caseSet.size() > 0 && !caseSet.contains(cases - 1))
+                continue;
+    		System.err.print(String.format("  Testcase #%d ... ", cases - 1));
+
+            if (doTest(packets, __answer))
+                passed++;
+	    }
+	    if (caseSet.size() > 0) cases = caseSet.size();
+        System.err.println(String.format("%nPassed : %d/%d cases", passed, cases));
+
+        int T = (int)(System.currentTimeMillis() / 1000) - 1470099860;
+        double PT = T / 60.0, TT = 75.0;
+        System.err.println(String.format("Time   : %d minutes %d secs%nScore  : %.2f points", T / 60, T % 60, 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT))));
+	}
+
+	static boolean doTest(String[] packets, int __expected) {
+		for (int i = 0; i < packets.length; i++) {
+			packets[i] = new String(packets[i]);
+		}
+		long startTime = System.currentTimeMillis();
+		Throwable exception = null;
+		EllysCandyBags instance = new EllysCandyBags();
+		int __result = 0;
+		try {
+			__result = instance.getSize(packets);
+		}
+		catch (Throwable e) { exception = e; }
+		double elapsed = (System.currentTimeMillis() - startTime) / 1000.0;
+
+		if (exception != null) {
+			System.err.println("RUNTIME ERROR!");
+			exception.printStackTrace();
+			return false;
+		}
+		else if (__result == __expected) {
+			System.err.println("PASSED! " + String.format("(%.2f seconds)", elapsed));
+			return true;
+		}
+		else {
+			System.err.println("FAILED! " + String.format("(%.2f seconds)", elapsed));
+			System.err.println("           Expected: " + __expected);
+			System.err.println("           Received: " + __result);
+			return false;
+		}
+	}
+
+	static class Reader {
+        private static final String dataFileName = "EllysCandyBags.sample";
+	    private static BufferedReader reader;
+
+	    public static String nextLine() {
+	        try {
+                if (reader == null) {
+                    reader = new BufferedReader(new InputStreamReader(new FileInputStream(dataFileName)));
+                }
+                return reader.readLine();
+	        }
+	        catch (IOException e) {
+	            System.err.println("FATAL!! IOException");
+	            e.printStackTrace();
+	            System.exit(1);
+	        }
+	        return "";
+	    }
+	}
+// CUT end
+}
