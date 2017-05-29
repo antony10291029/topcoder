222e7f4645a6d5d174d4ba02b0805f8d4196dbd5
Qingyu Li <liqingyu.lqy@gmail.com>
2017-05-29 10:30:39 +0800

    feat: trick, Time: O(N^3), space: O(N)

---
+++ SRM 670/Bracket107.cpp
@@ -0,0 +1,138 @@
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <typeinfo>
+#include <fstream>
+
+using namespace std;
+#define rep(i,n) for(int i = 0; i < (n); ++i)
+class Bracket107 {
+    public:
+    bool ok(string s) {
+        int sum = 0;
+        rep(i,s.size()) {
+            if (s[i] == '(') ++sum;
+            else --sum;
+            if (sum < 0) return false;
+        }
+        return true;
+    }
+    int yetanother(string s) {
+        set<string> v;
+        int n = s.size();
+        rep(i,n) {
+            string cur = s.substr(0,i) + s.substr(i+1);
+            char insert_ch = s[i];
+            rep(j, cur.size()) {
+                string ccur = cur.substr(0,j) + insert_ch + cur.substr(j);
+                if (ok(ccur)) {
+                    v.insert(ccur);
+                }
+            }
+        }
+        return (v.size() - 1);
+    }
+};
+
+// CUT begin
+ifstream data("Bracket107.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(string s, int __expected) {
+    time_t startClock = clock();
+    Bracket107 *instance = new Bracket107();
+    int __result = instance->yetanother(s);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        string s;
+        from_stream(s);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(s, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1495965871;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "Bracket107 (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
