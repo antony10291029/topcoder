160eb16130b180c985900bdf01cc27174ce4d64c
Qingyu Li <liqingyu.lqy@gmail.com>
2017-07-25 09:49:04 +0800

    feat: DP + trick, Time: O(M*log(M)), Space: O(M), M<= 50

---
+++ SRM 650/TaroFillingAStringDiv1.java
@@ -0,0 +1,98 @@
+import java.io.*;
+import java.util.*;
+
+public class TaroFillingAStringDiv1 {
+	public int getNumber(int N, int[] position, String value) {
+		return 0;
+	}
+
+// CUT begin
+	public static void main(String[] args){
+		System.err.println("TaroFillingAStringDiv1 (250 Points)");
+		System.err.println();
+		HashSet<Integer> cases = new HashSet<Integer>();
+        for (int i = 0; i < args.length; ++i) cases.add(Integer.parseInt(args[i]));
+        runTest(cases);
+	}
+
+	static void runTest(HashSet<Integer> caseSet) {
+	    int cases = 0, passed = 0;
+	    while (true) {
+	    	String label = Reader.nextLine();
+	    	if (label == null || !label.startsWith("--"))
+	    		break;
+
+            int N = Integer.parseInt(Reader.nextLine());
+            int[] position = new int[Integer.parseInt(Reader.nextLine())];
+            for (int i = 0; i < position.length; ++i)
+                position[i] = Integer.parseInt(Reader.nextLine());
+            String value = Reader.nextLine();
+            Reader.nextLine();
+            int __answer = Integer.parseInt(Reader.nextLine());
+
+            cases++;
+            if (caseSet.size() > 0 && !caseSet.contains(cases - 1))
+                continue;
+    		System.err.print(String.format("  Testcase #%d ... ", cases - 1));
+
+            if (doTest(N, position, value, __answer))
+                passed++;
+	    }
+	    if (caseSet.size() > 0) cases = caseSet.size();
+        System.err.println(String.format("%nPassed : %d/%d cases", passed, cases));
+
+        int T = (int)(System.currentTimeMillis() / 1000) - 1500858979;
+        double PT = T / 60.0, TT = 75.0;
+        System.err.println(String.format("Time   : %d minutes %d secs%nScore  : %.2f points", T / 60, T % 60, 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT))));
+	}
+
+	static boolean doTest(int N, int[] position, String value, int __expected) {
+        value = new String(value);
+		long startTime = System.currentTimeMillis();
+		Throwable exception = null;
+		TaroFillingAStringDiv1 instance = new TaroFillingAStringDiv1();
+		int __result = 0;
+		try {
+			__result = instance.getNumber(N, position, value);
+		}
+		catch (Throwable e) { exception = e; }
+		double elapsed = (System.currentTimeMillis() - startTime) / 1000.0;
+
+		if (exception != null) {
+			System.err.println("RUNTIME ERROR!");
+			exception.printStackTrace();
+			return false;
+		}
+		else if (__result == __expected) {
+			System.err.println("PASSED! " + String.format("(%.2f seconds)", elapsed));
+			return true;
+		}
+		else {
+			System.err.println("FAILED! " + String.format("(%.2f seconds)", elapsed));
+			System.err.println("           Expected: " + __expected);
+			System.err.println("           Received: " + __result);
+			return false;
+		}
+	}
+
+	static class Reader {
+        private static final String dataFileName = "TaroFillingAStringDiv1.sample";
+	    private static BufferedReader reader;
+
+	    public static String nextLine() {
+	        try {
+                if (reader == null) {
+                    reader = new BufferedReader(new InputStreamReader(new FileInputStream(dataFileName)));
+                }
+                return reader.readLine();
+	        }
+	        catch (IOException e) {
+	            System.err.println("FATAL!! IOException");
+	            e.printStackTrace();
+	            System.exit(1);
+	        }
+	        return "";
+	    }
+	}
+// CUT end
+}
